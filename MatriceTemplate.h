75e7e837b26de283aaefe016bf6a759e0ddcc0b5
christianpratellesi <christian.pratellesi@virgilio.it>
2018-07-11 17:36:48 +0200

    Primo Commit

---
+++ MatriceTemplate.h
@@ -0,0 +1,29 @@
+//
+// Created by Christian Pratellesi on 11/07/18.
+//
+
+#ifndef PROGETTOMATRICITEMPLATE_MATRICETEMPLATE_H
+#define PROGETTOMATRICITEMPLATE_MATRICETEMPLATE_H
+
+template<typename T>
+class MatriceTemplate{
+public:
+    MatriceTemplate(int r, int c):righe(r),colonne(c){
+        if(righe<1)
+            righe=1;
+        if(colonne<1)
+            colonne=1;
+        matrice=new T[righe*colonne];
+        for(int i=0;i<righe;i++){
+            for(int j=0;j<colonne;j++){
+                matrice[i*colonne+j]=0;
+            }
+        }
+    };
+
+private:
+    T* matrice;
+    int righe,colonne;
+};
+
+#endif //PROGETTOMATRICITEMPLATE_MATRICETEMPLATE_H
