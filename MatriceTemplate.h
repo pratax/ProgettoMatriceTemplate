75120a69613ffa13c33363e62da4bdcd5b5bc699
christianpratellesi <christian.pratellesi@virgilio.it>
2018-07-11 17:53:27 +0200

    Aggiunti getter e setter dei valori della matrice

--- MatriceTemplate.h
+++ MatriceTemplate.h
@@ -5,14 +5,16 @@
 #ifndef PROGETTOMATRICITEMPLATE_MATRICETEMPLATE_H
 #define PROGETTOMATRICITEMPLATE_MATRICETEMPLATE_H
 
+#include <stdexcept>
+
 template<typename T>
 class MatriceTemplate{
 public:
     MatriceTemplate(int r, int c):righe(r),colonne(c){
-        if(righe<1)
-            righe=1;
-        if(colonne<1)
-            colonne=1;
+        if(righe<0)
+            righe=0;
+        if(colonne<0)
+            colonne=0;
         matrice=new T[righe*colonne];
         for(int i=0;i<righe;i++){
             for(int j=0;j<colonne;j++){
@@ -33,6 +35,32 @@ public:
     int getColonne() const {
         return colonne;
     }
+
+    T getValue(int r, int c) const {
+        if(r<0)
+            throw std::out_of_range("Il numero della riga non può essere minore di 0");
+        if(r>=righe)
+            throw std::out_of_range("Il numero della riga supera le dimensioni della matrice");
+        if(c<0)
+            throw std::out_of_range("Il numero della colonna non può essere minore di 0");
+        if(c>=colonne)
+            throw std::out_of_range("Il numero della colonna supera le dimensioni della matrice");
+        return matrice[r*colonne+c];
+    }
+
+    void setValue(int r, int c, T& value) {
+        if(r<0)
+            throw std::out_of_range("Il numero della riga non può essere minore di 0");
+        if(r>=righe)
+            throw std::out_of_range("Il numero della riga supera le dimensioni della matrice");
+        if(c<0)
+            throw std::out_of_range("Il numero della colonna non può essere minore di 0");
+        if(c>=colonne)
+            throw std::out_of_range("Il numero della colonna supera le dimensioni della matrice");
+        matrice[r*colonne+c]=value;
+    }
+
+
 
 
 
